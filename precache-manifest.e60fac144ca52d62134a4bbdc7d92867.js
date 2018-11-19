f2ff25b4e590eddc80f659a64f3afac2a44c11b1
Guillaume R <guillaumer@bam.tech>
2018-11-19 16:10:02 +0100

    Updates

---
+++ precache-manifest.e60fac144ca52d62134a4bbdc7d92867.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d3e4b803bdd7a07feac9",
+    "url": "/charts-victory/static/js/runtime~main.d3e4b803.js"
+  },
+  {
+    "revision": "b68cdaa22f3da5f77e49",
+    "url": "/charts-victory/static/js/main.b68cdaa2.chunk.js"
+  },
+  {
+    "revision": "e6a97468fb4aecda31d9",
+    "url": "/charts-victory/static/js/1.e6a97468.chunk.js"
+  },
+  {
+    "revision": "b68cdaa22f3da5f77e49",
+    "url": "/charts-victory/static/css/main.a80c74c6.chunk.css"
+  },
+  {
+    "revision": "3a1d2e1e06aaaaf8f4faf03f78eed104",
+    "url": "/charts-victory/index.html"
+  }
+];
